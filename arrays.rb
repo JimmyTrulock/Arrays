ce90e1c41aac8521a1d04af65cd124868a257b34
Jimmy Trulock <ttrulock23@gmail.com>
2023-06-21 15:20:18 -0600

    add demonstration of understanding and a new method.

--- arrays.rb
+++ arrays.rb
@@ -14,3 +14,14 @@ puts prices.pop
 
 # this line will add something (in this case kohls=false) to the last element in the stores array.
 puts stores.push(kohls=false)
+
+
+# because an argument is being made, this line will remove the first 2 elements in the array.
+puts lucky_nums.shift(2)
+# this will add 2 more numbers to the end of the array.
+puts lucky_nums.push(43, 99)
+# this will print the array, now with a new set of numbers
+puts lucky_nums
+
+# this will delete anything in the array that matches the argument and return it.
+puts prices.delete(19.95)
